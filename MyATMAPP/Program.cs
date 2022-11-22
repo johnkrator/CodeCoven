b0e23a1566dd07dc9162e08629a8300a90c00c2d
john <chukwuchidieberejohn@gmail.com>
2022-11-22 14:38:03 +0100

    initial commit

---
+++ MyATMAPP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyATMAPP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Jhon");
+        }
+    }
+}
