f2f6dbd867b651555fa4cbea23d3f81c50e387a1
john <chukwuchidieberejohn@gmail.com>
2022-11-22 13:06:59 +0100

    initial commit

--- MyATM/Program.cs
+++ MyATM/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyATM
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Title = "My ATM APP";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Basic comments
+            Console.WriteLine("\n------------------------------------Hello there! Good day and welcome to my ATM App------------------------------------");
+            Console.ReadLine();
+
+        }
+    }
+}
