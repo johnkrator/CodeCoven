615ad1d531edb37b03fdcdca885bf39f1a8adc01
john <chukwuchidieberejohn@gmail.com>
2022-11-22 14:21:31 +0100

    initial commit

---
+++ MyATM/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyATM
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Learning to write C#");
+        }
+    }
+}
