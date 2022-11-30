a17b09be922f900a5a917ef4ac5e0c3ea3a0bc75
john <chukwuchidieberejohn@gmail.com>
2022-11-30 08:41:03 +0100

    Updated .gitignore, indexLayout.xml, and 3 more files...

---
+++ MusicPlayer/MusicPlayer/Playlist.cs
@@ -0,0 +1,7 @@
+namespace MusicPlayer
+{
+    public class Playlist
+    {
+        
+    }
+}
---
+++ MusicPlayer/MusicPlayer/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicPlayer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
