2d9af37bb7bf478d7d7a4def7b013306145ed28f
john <chukwuchidieberejohn@gmail.com>
2022-12-15 09:10:00 +0100

    Updated README.md

--- TaskManager/TaskManager/Processes.cs
+++ TaskManager/TaskManager/Processes.cs
@@ -7,10 +7,12 @@ namespace TaskManager
 {
     public class Processes
     {
+        private static ThreadStart _threadStart;
+
         public static void SelectCommand()
         {
             Console.Write(
-                "\nPlease select a command to continue: \n1. List all running task\n2. Start a task\n3. Create a custom process\n4. Create a custom thread and a background thread\n5. Check if a thread is alive\n*******************************************\n\n");
+                "\nPlease select a command to continue: \n1. List all running task\n2. Start a task\n3. Create a custom process\n4. Create a custom thread\n5. View current thread list\n6. Check thread current status\n*******************************************\n\n");
             var option = int.Parse(Console.ReadLine());
 
             switch (option)
@@ -22,13 +24,16 @@ namespace TaskManager
                     StartAndKillATask();
                     break;
                 case 3:
-                    CreateACustomProcess();
+                    CreateACustomProcess(fileName: "random");
                     break;
                 case 4:
-                    CreateCustomThreadAndBackgroundThread();
+                    CreateCustomThread(threadName: "task");
                     break;
                 case 5:
-                    IsThreadAliveOrSleeping();
+                    ViewCurrentThreadList();
+                    break;
+                case 6:
+                    CheckThreadState();
                     break;
                 default:
                     Console.WriteLine("Invalid entry. Please enter a valid option to continue.");
@@ -63,14 +68,14 @@ namespace TaskManager
         // 2. Start and kill all running browser task
         public static void StartAndKillATask()
         {
-            Process proc = null;
+            Process process;
 
             // Start a process
             try
             {
                 // Open chrome browser
-                proc = Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", "www.codewars.com");
-                Console.WriteLine(proc.ProcessName);
+                process = Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", "www.codewars.com");
+                Console.WriteLine(process.ProcessName);
             }
             catch (InvalidOperationException e)
             {
@@ -79,13 +84,13 @@ namespace TaskManager
             }
 
             // Kills Task flow
-            Console.WriteLine($"-> Hit enter to kill: {proc.ProcessName}");
+            Console.WriteLine($"-> Hit enter to kill: {process.ProcessName}");
             Console.ReadLine();
 
             // Kills all of chrome.exe processes
             try
             {
-                foreach (var p in Process.GetProcessesByName(proc.ProcessName))
+                foreach (var p in Process.GetProcessesByName(process.ProcessName))
                 {
                     p.Kill();
                 }
@@ -98,76 +103,48 @@ namespace TaskManager
         }
 
         // 3. Creates a custom process
-        public static void CreateACustomProcess()
+        public static void CreateACustomProcess(string fileName)
         {
-            Process theProc = null;
-
             try
             {
-                theProc = Process.GetProcessById(Process.GetCurrentProcess().Id);
-                Console.WriteLine(theProc.ProcessName);
+                Process newProcess = new Process();
+                newProcess.StartInfo.FileName = fileName;
 
-                Console.WriteLine($"\nHere are the process used by: {theProc.ProcessName}");
-                ProcessThreadCollection theThreads = theProc.Threads;
-
-                foreach (ProcessThread pt in theThreads)
-                {
-                    var info =
-                        $"-> Thread ID: {pt.Id}\tStart time: {pt.StartTime.ToShortTimeString()}\tPriority: {pt.PriorityLevel}\tName: {pt.PriorityBoostEnabled}";
-                    Console.WriteLine(info);
-                }
-
-                Console.WriteLine("*****************************\n");
+                Console.WriteLine($"\nSuccessfully created and added {fileName} process!");
             }
-            catch (ArgumentException e)
+            catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e);
                 throw;
             }
         }
 
         // 4. Creates a custom thread
-        public static void CreateCustomThreadAndBackgroundThread()
+        public static void CreateCustomThread(string threadName)
         {
-            Console.WriteLine("\n**********The Amazing Thread App*************\n");
-            Console.WriteLine("Do you want [1] or [2] threads?");
-
-            var threadCount = Console.ReadLine();
-            Test(threadCount);
+            var thread = new Thread(_threadStart);
+            threadName = threadName;
+            thread.Start();
+            Console.WriteLine($"{threadName} has been created");
         }
 
-        private static void Test(string input)
+        // *****************************
+        //method to view list of current threads
+        public static void ViewCurrentThreadList()
         {
-            if (input == "1")
+            ProcessThreadCollection currentThreads = Process.GetCurrentProcess().Threads;
+
+            foreach (ProcessThread thread in currentThreads)
             {
-                PrintNumbers();
-            }
-            else
-            {
-                ThreadStart threadStart = new ThreadStart(PrintNumbers);
-                Thread backgroundThread = new Thread(threadStart);
-                backgroundThread.Name = "Secondary";
-                backgroundThread.Start();
+                Console.WriteLine($"\n Thread id: {thread.Id}");
             }
         }
 
-        private static void PrintNumbers()
+        public static void CheckThreadState()
         {
-            Console.WriteLine($"\n-> Executing PrintNumbers() {Thread.CurrentThread.Name}");
-
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-                Thread.Sleep(2000);
-            }
-
-            Console.WriteLine();
-        }
-
-        // 5. Checks if a thread isAlive
-        public static void IsThreadAliveOrSleeping()
-        {
-            Console.WriteLine("Is thread sleeping");
+            Thread thread;
+            thread = Thread.CurrentThread;
+            Console.WriteLine($"Is thread alive? : {thread.IsAlive}");
         }
 
         public void PrintMessage()
---
+++ TaskManager/TaskManager/README.md
@@ -0,0 +1,7 @@
+Welcome to my console task management app. The app has the following features:
+1. An ability to list all running tasks
+2. An ability to start and kill a process 
+3. Create a custom process
+4. Create a custom thread 
+5. View current thread list(s)
+6. Check thread current status
