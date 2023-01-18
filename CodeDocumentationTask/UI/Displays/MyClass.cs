bdc451e9186fd33669230d768d05ed563216fddb
john <chukwuchidieberejohn@gmail.com>
2023-01-18 09:48:59 +0100

    Updated CodeDocumentationTask.dll

--- CodeDocumentationTask/UI/Displays/MyClass.cs
+++ CodeDocumentationTask/UI/Displays/MyClass.cs
@@ -10,28 +10,28 @@ public class MyClass
         Gender = gender;
     }
 
-    [Document("Refers to how long this person has been on earth", "Takes in an integer")]
+    [Document("Describes a persons age", "Takes in an integer")]
     public int Age { get; set; }
 
-    [Document("Obvious biological traits associated with this person", "Takes in an enum")]
+    [Document("Physical attributes that define the person's sex", "Takes in an enum")]
     public GenderEnum Gender { get; set; }
 
 
-    [Document("Provides valid gender options a person can be")]
+    [Document("Showcases the person's defined gender")]
     public enum GenderEnum
     {
         Male,
-        Female
+        Female,
+        SheMale
     }
 
-    [Document("Makes a rather obvious sentence with the parameters provided", "Age and gender",
-        "Outputs a sentence, otherwise known as string")]
+    [Document("Displays mentions", "Age and gender", "Returns mentions as a string argument")]
     public void MakeSentenceWithPerson(int age, GenderEnum gender)
     {
         this.Gender = gender;
         this.Age = age;
 
-        if (GenderEnum.Male == gender) Console.WriteLine("We have a {0} year old male", age);
-        else Console.WriteLine("We have a {0} year old female", age);
+        if (GenderEnum.Male == gender) Console.WriteLine($"We have a {age} year old male");
+        else Console.WriteLine($"We have a {age} year old female");
     }
 }
